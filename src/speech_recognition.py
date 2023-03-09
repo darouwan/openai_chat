a055f456bfbfb24d9c115356ee552777d173cbdc
Gabriel Chen <k-2feng@hotmail.com>
2023-03-09 21:51:54 +0800

    avoid echo

--- src/speech_recognition.py
+++ src/speech_recognition.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import azure.cognitiveservices.speech as speechsdk
 import conversation
 from loguru import logger
@@ -11,63 +13,68 @@ speech_key = os.environ.get('SPEECH_KEY')
 service_region = os.environ.get('SPEECH_REGION')
 
 
-def speech_recognize_continuous_async_from_microphone():
-    """performs continuous speech recognition asynchronously with input from microphone"""
+def speech_recognize_once_from_mic():
+    """performs one-shot speech recognition from the default microphone"""
+    logger.info("Please start your conversation:")
+    # <SpeechRecognitionWithMicrophone>
     speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
     speech_config.speech_recognition_language = 'zh-CN'
-    speech_config.speech_synthesis_voice_name = 'zh-CN-XiaochenNeural'
+    speech_config.speech_synthesis_voice_name = 'zh-CN-YunfengNeural'
+    """
+    	zh-CN-XiaochenNeural (Female)
+        zh-CN-XiaohanNeural (Female)
+        zh-CN-XiaomengNeural (Female)
+        zh-CN-XiaomoNeural (Female)
+        zh-CN-XiaoqiuNeural (Female)
+        zh-CN-XiaoruiNeural (Female)
+        zh-CN-XiaoshuangNeural (Female, Child)
+        zh-CN-XiaoxiaoNeural (Female)
+        zh-CN-XiaoxuanNeural (Female)
+        zh-CN-XiaoyanNeural (Female)
+        zh-CN-XiaoyiNeural (Female)
+        zh-CN-XiaoyouNeural (Female, Child)
+        zh-CN-XiaozhenNeural (Female)
+        zh-CN-YunfengNeural (Male)
+        zh-CN-YunhaoNeural (Male)
+        zh-CN-YunjianNeural (Male)
+        zh-CN-YunxiaNeural (Male)
+        zh-CN-YunxiNeural (Male)
+        zh-CN-YunyangNeural (Male)
+        zh-CN-YunyeNeural (Male)
+        zh-CN-YunzeNeural (Male)
+    """
 
     audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
     # The default language is "en-us".
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
     speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-    done = False
+    # Starts speech recognition, and returns after a single utterance is recognized. The end of a
+    # single utterance is determined by listening for silence at the end or until a maximum of 15
+    # seconds of audio is processed. It returns the recognition text as result.
+    # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
+    # shot recognition like command or query.
+    # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
+    while True:
 
-    def recognizing_cb(evt: speechsdk.SpeechRecognitionEventArgs):
-        print('RECOGNIZING: {}'.format(evt))
+        result = speech_recognizer.recognize_once()
 
-    def recognized_cb(evt: speechsdk.SpeechRecognitionEventArgs):
-        print('RECOGNIZED...: {}'.format(evt))
-        response = conv.send_chat(evt.result.text)
-        logger.info(response)
-        response = "你好， 大肉丸。 " + response
-        speech_synthesis_result = speech_synthesizer.speak_text_async(response)
-        f = speech_synthesis_result.get()
-
-
-    def stop_cb(evt: speechsdk.SessionEventArgs):
-        """callback that signals to stop continuous recognition"""
-        print('CLOSING on {}'.format(evt))
-        nonlocal done
-        done = True
-
-    # Connect callbacks to the events fired by the speech recognizer
-    speech_recognizer.recognizing.connect(recognizing_cb)
-    speech_recognizer.recognized.connect(recognized_cb)
-    speech_recognizer.session_stopped.connect(stop_cb)
-    speech_recognizer.canceled.connect(stop_cb)
-
-    # Perform recognition. `start_continuous_recognition_async asynchronously initiates continuous recognition operation,
-    # Other tasks can be performed on this thread while recognition starts...
-    # wait on result_future.get() to know when initialization is done.
-    # Call stop_continuous_recognition_async() to stop recognition.
-    result_future = speech_recognizer.start_continuous_recognition_async()
-
-    result_future.get()  # wait for voidfuture, so we know engine initialization is done.
-    print('Continuous Recognition is now running, say something.')
-
-    while not done:
-        # No real sample parallel work to do on this thread, so just wait for user to type stop.
-        # Can't exit function or speech_recognizer will go out of scope and be destroyed while running.
-        print('type "stop" then enter when done')
-        stop = input()
-        if (stop.lower() == "stop"):
-            print('Stopping async recognition.')
-            speech_recognizer.stop_continuous_recognition_async()
-            break
-
-    print("recognition stopped, main thread can exit now.")
+        # Check the result
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            logger.info("Recognized: {}".format(result.text))
+            response = conv.send_chat(result.text)
+            logger.info(response)
+            response = "" + response
+            speech_synthesis_result = speech_synthesizer.speak_text_async(response)
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            print("No speech could be recognized")
+        elif result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = result.cancellation_details
+            print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print("Error details: {}".format(cancellation_details.error_details))
+        time.sleep(0.1)
+    # </SpeechRecognitionWithMicrophone>
 
 
-speech_recognize_continuous_async_from_microphone()
+speech_recognize_once_from_mic()
